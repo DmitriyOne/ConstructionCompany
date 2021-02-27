f3740777e494e4243b4d57194c15d9f302bbdf22
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2021-02-27 13:05:41 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+// import './import/swiper';
+import './import/ui';
+// import objectFitImages from 'object-fit-images';
+
+$(document).ready(function() {
+  $('.header__burger').click(function(e) { 
+    $('.header__burger, .header__menu, .wrap, .callOur__nav ').toggleClass('active');
+    if($('.callOur__nav')=== true) {
+      dis;
+    }
+  });
+});
+
+
+// objectFitImages();
