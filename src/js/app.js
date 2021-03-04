179896d95892912b1c9023dc359248ba02b32144
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2021-03-04 13:54:27 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,11 @@ import './import/ui';
 $(document).ready(function() {
   $('.header__burger').click(function(e) {
     $('.header__burger, .header__menu, .wrap, .callOur__nav ').toggleClass('active');
-    if ($('.callOur__nav') === true) {
-      dis;
-    }
+    
   });
 });
 
 
-
-//show popup
 $('.btn__call input').click(function(event) {
   event.preventDefault();
   show();
@@ -22,11 +18,18 @@ $('.contact__header input').click(function(event) {
   event.preventDefault();
   show();
 });
+$('.header__body input').click(function(event) {
+  event.preventDefault();
+  show();
+});
+
+
 
 $('.popup__bg').click(function(event) {
   event.preventDefault();
   hide();
 });
+
 
 
 
